df49e1874248027162da9a85761478a23bc64c1f
miyokidev <bgrin@miyoki.ch>
2022-03-29 01:43:33 +0200

    commented most part of the code, added the game object to room, handle when player start a game, handle when player play a cell, found a solution to keep player connected even tho it disconnect/reconnect everytime you refresh page or navigate

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ const socketPort = process.env.SOCKET_PORT;
 
 var server = require('http').createServer(socketApp);
 var io = require('socket.io')(server, {
-    cors: {    
+    cors: {
         origin: "*",
     }
 });
@@ -41,73 +41,166 @@ var io = require('socket.io')(server, {
 let rooms = [];
 
 io.on('connection', (socket) => {
-    // On vérifie si l'utilisateur nous renseigne un token existant dans la mémoire.
+    // On vérifie si l'utilisateur nous renseigne un token existant dans la session.
     if (store.get(socket.handshake.auth.token) != null) {
         console.log('Connected: ' + socket.id);
         var token = socket.handshake.auth.token;
-        // Refresh socket id of user by token
+
+        // le socket.id est différent à chaque fois qu'un utilisateur change de page ou 
+        // raffraichit sa page c'est pourquoi on se sert du token pour vérifier son identité
+        // et on restock son nouveau socket.id dans la session.
         var user = store.get(token);
         user.id = socket.id;
         store.set(token, user);
 
+        //---------------------------------------------------------------
+        // Gérer quand l'utilisateur change de page ou rafraichit sa page
+        //----------------------------------------------------------------
+
+        // On vérifie à l'aide de son token si il appartenait à une salle
+        // roomPlayer est égal à null si il était dans aucune salle
         var roomPlayer = isPlayerInARoom(socket, rooms);
 
-        if (roomPlayer != null) {
-            socket.join(roomPlayer.id);
-            emitPlayerList(io, roomPlayer);
+        if (roomPlayer != null) { // dans le cas où il est dans une salle
+            socket.join(roomPlayer.id); // On le fait re-rejoindre sa salle socket.io
+            emitPlayerList(io, roomPlayer); // On lui donne la liste des personnes dans la salle
+
+            // Si une partie était en cours on lui renvoit les informations du match en cours
+            if (roomPlayer.game.inProgress) {
+                io.to(roomPlayer.id).emit('isPlayer1Turn', roomPlayer.game.isP1Turn); // A qui est le tour
+                // emit grid
+            }
         }
 
+        // Déconnexion s'éxécute à chaque fois que l'utilisateur ferme sa page, change de page, raffraichit sa page
         socket.on('disconnect', () => {
-            var roomPlayer = isPlayerInARoom(socket, rooms);
-            if (roomPlayer != null) {
-                socket.leave(roomPlayer.id);
-            }
             console.log('Disconnected: ' + socket.id);
         });
-    
-        socket.on('createRoom', () => {
-            var token = socket.handshake.auth.token;
 
+        //-----------------------------------------------------------------
+        // ROOMS
+        //-----------------------------------------------------------------
+        /* Les rooms sont gérés de manière stateless tout dans le code sans base de données
+        *  à l'aide d'une liste de rooms.
+        */
+        socket.on('createRoom', () => {
+            // Si le joueur est déjà dans une room on le fait quitter
+            if (roomPlayer != null) {
+                rooms = handlePlayerLeave(io, socket, rooms);
+            }
+
+            // Création d'une room avec les infos par défaut
             const room = {
-                id: generateRandomId(),
-                players: [{token : token, name: store.get(token).name}],
+                id: generateRandomId(), // id unique pour chaque room permettant de les différencier
+                players: [ // tableau de joueur pour y stocker les 2 joueurs participants
+                    {
+                        token: token, // token du joueur obtenu lors de la connexion
+                        name: store.get(token).name // pseudo du joueur stocké en session avec son id de socket
+                    }
+                ],
+                game: {
+                    inProgress: false, // état de la partie pas encore démarré
+                    grid: [ // tableau de 3x3 null par défaut
+                        [null, null, null],
+                        [null, null, null],
+                        [null, null, null]
+                    ],
+                    isP1Turn: true, // premier tour pour le joueur 1 par défaut
+                }
             };
-    
-            rooms.push(room);
+
+            rooms.push(room); // Ajoutez la room dans la liste des rooms
             socket.join(room.id);
             emitPlayerList(io, room);
             io.to(room.id).emit("id", {
                 id: room.id
             });
         });
-    
+
         socket.on('joinRoom', event => {
-            var token = socket.handshake.auth.token;
-            for (let i = 0; i < rooms.length; i++) {
-                let currentRoom = rooms[i];
-                if (currentRoom.id == event.id) {
-                    if (currentRoom.players.length < 2) {
-                        currentRoom.players.push({token: token, name: store.get(token).name});
-                        socket.join(currentRoom.id);
-                        io.to(socket.id).emit('joinedRoom', {
-                            id: currentRoom.id
-                        });
-                        emitPlayerList(io, currentRoom);
+            // Si le joueur est déjà dans une room on le fait quitter
+            if (roomPlayer != null) {
+                rooms = handlePlayerLeave(io, socket, rooms);
+            }
+
+            if (rooms.length > 0) {
+                for (let i = 0; i < rooms.length; i++) {
+                    let currentRoom = rooms[i];
+                    if (currentRoom.id == event.id) {
+                        if (currentRoom.players.length < 2) {
+                            currentRoom.players.push({ token: token, name: store.get(token).name });
+                            socket.join(currentRoom.id);
+                            io.to(socket.id).emit('joinedRoom', {
+                                id: currentRoom.id
+                            });
+                            emitPlayerList(io, currentRoom);
+                        } else {
+                            io.to(socket.id).emit('exception', { errorMessage: `Le salon est plein` });
+                        }
                     } else {
-                        io.to(socket.id).emit('exception', {errorMessage: `Le salon est plein`});
+                        io.to(socket.id).emit('exception', { errorMessage: `Aucun salon n'a été trouvé avec le code renseigné` });
                     }
-                } else {
-                    io.to(socket.id).emit('exception', {errorMessage: `Aucun salon n'a été trouvé avec le code renseigné`});
                 }
+            } else {
+                io.to(socket.id).emit('exception', { errorMessage: `Aucun salon n'a été trouvé avec le code renseigné` });
             }
         });
-    
+
         socket.on('leaveRoom', () => {
             rooms = handlePlayerLeave(io, socket, rooms);
         });
+        //----------------------------------------------------------------------
+
+        socket.on('startGame', () => {
+            if (roomPlayer != null) {
+                if (token == roomPlayer.players[0].token) {
+                    if (roomPlayer.players.length == 2) {
+                        io.to(roomPlayer.id).emit('startedGame', {
+                            id: roomPlayer.id,
+                        });
+
+                        rooms.map(r => r.id == roomPlayer.id ? r.game.inProgress = true : r);
+                    } else {
+                        io.to(socket.id).emit('exception', { errorMessage: `Il faut être 2 joueurs pour lancer une partie` });
+                    }
+                } else {
+                    io.to(socket.id).emit('exception', { errorMessage: `Vous n'êtes pas le chef du salon` });
+                }
+            } else {
+                io.to(socket.id).emit('exception', { errorMessage: `Vous n'êtes dans aucun salon connu` });
+            }
+        });
+
+        socket.on('play', cell => {
+            if (roomPlayer != null) {
+                if (roomPlayer.game.inProgress) {
+                    let cellIndexes = cell.split(';');
+
+                    if (roomPlayer.game.isP1Turn) {
+                        if (roomPlayer.players[0].token == token) {
+                            rooms.map(r => r.id == roomPlayer.id ? r.game.grid[cellIndexes[0]][cellIndexes[1]] = "P1" : r);
+
+
+                        } else {
+                            io.to(socket.id).emit('exception', { errorMessage: `Ce n'est pas votre tour de jouer` });
+                        }
+                    } else {
+                        if (roomPlayer.players[1].token == token) {
+                            rooms.map(r => r.id == roomPlayer.id ? r.game.grid[cellIndexes[0]][cellIndexes[1]] = "P2" : r);
+
+
+                        } else {
+                            io.to(socket.id).emit('exception', { errorMessage: `Ce n'est pas votre tour de jouer` });
+                        }
+                    }
+                }
+            } else {
+                io.to(socket.id).emit('exception', { errorMessage: `Vous n'êtes dans aucun salon connu` });
+            }
+        });
     } else {
+        io.to(socket.id).emit('expiredToken');
         socket.disconnect();
-        io.to(socket.id).emit('exception', {errorMessage: `Token non valide`});
     }
 });
 
